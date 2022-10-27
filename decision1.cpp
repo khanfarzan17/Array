35d14c5887f6be2b84a1c381b0be3ac91cca6310
khanfarzan17 <khanfarzan200@gmail.com>
2022-10-27 23:07:12 +0530

    basic progam

---
+++ decision1.cpp
@@ -0,0 +1,24 @@
+//nested if//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    
+    if(x==y){
+        cout<<"both are same";
+        
+    }
+    else{
+        if(x>y){
+            cout<<"x is greater than y";
+        }
+        else{
+            cout<<"y is greater than x";
+            
+        }
+    }
+    return 0;
+    
+}
