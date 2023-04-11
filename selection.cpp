e2d9670e36ccf1888ee0278e2f770ab599188cbf
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-04-11 17:07:44 +0530

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -5,10 +5,10 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0;i<=n;i++){
+    for(int i=1;i<=n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<=n-1;i++){
+    for(int i=1;i<=n;i++){
         for(int j=i+1;j<=n;j++){
             if(arr[j]<arr[i]){
                 int temp=arr[j];
